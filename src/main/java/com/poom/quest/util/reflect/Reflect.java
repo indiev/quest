93d2f8c9bdc707915822cbc59b2ff23ef754168f
ehnawh <ehnawh@gmail.com>
2016-07-18 07:47:11 +0900

    util.Reflect Class 추가

---
+++ src/main/java/com/poom/quest/util/reflect/Reflect.java
@@ -0,0 +1,19 @@
+package com.poom.quest.util.reflect;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Reflect {
+
+	static public Method getMethod(Class<?> clazz, String keyword) {
+		for(Method method : clazz.getMethods())
+			if(method.getName().equalsIgnoreCase(keyword)) return method;
+		return null;
+	}
+	
+	static public Field getField(Class<?> clazz, String keyword) {
+		for(Field field: clazz.getFields())
+			if(field.getName().equalsIgnoreCase(keyword)) return field;
+		return null;
+	}
+}
