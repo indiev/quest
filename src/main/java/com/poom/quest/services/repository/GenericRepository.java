ae5bd55eddbd7a1339b94bd2918a6bca6695481f
ehnawh <ehnawh@gmail.com>
2016-12-12 17:22:59 +0900

    genericRepository에 queryDSL 적용

--- src/main/java/com/poom/quest/services/repository/GenericRepository.java
+++ src/main/java/com/poom/quest/services/repository/GenericRepository.java
@@ -3,7 +3,6 @@ package com.poom.quest.services.repository;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -91,30 +90,11 @@ public abstract class GenericRepository<T extends Domain, ID extends Serializabl
 		return em.createNativeQuery(SELECT_ALL_SQL, domainClass).getResultList();
 	}
 	
-	protected CriteriaQuery<T> setSort(Pageable pageable, CriteriaQuery<T> criteriaQuery, Root<T> model) {
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-		List<Order> orders = new ArrayList<>();
-		for(Sort.Order sortOrder : pageable.getSort())
-			if(sortOrder.getDirection().equals(Sort.Direction.ASC))
-				orders.add(criteriaBuilder.asc(model.get(sortOrder.getProperty())));
-			else
-				orders.add(criteriaBuilder.desc(model.get(sortOrder.getProperty())));
-		criteriaQuery.orderBy(orders);
-		return criteriaQuery;
-	}
-	
-	protected Query setPageable(Pageable pageable, Query query) {
-		return query.setMaxResults(pageable.getPageSize())
-				.setFirstResult(pageable.getOffset());
-	}
-	
 	public List<T> list(Pageable pageable) {
 		return (List<T>) querydsl.applyPagination(pageable, from(entityPath)).fetch();
 	}
-	
-	public List<T> listByKeys(Map<String, Object> params, Pageable pageable) {
-		JPQLQuery<T> query = (JPQLQuery<T>) from(entityPath);
-		
+
+	public JPQLQuery<T> listByKeys(Map<String, Object> params, JPQLQuery<T> query) {
 		for(String key : params.keySet()) {
 			Field field = Reflect.getField(domainClass, key);
 			if(field == null) { //검색되면 안되는 값. 필터링 해야 됨
@@ -132,19 +112,22 @@ public abstract class GenericRepository<T extends Domain, ID extends Serializabl
 					query.where(entityPath.get(key+"Id").eq(params.get(key)));
 				}
 			} else if(Set.class.isAssignableFrom(field.getType())) { //Type이 List일 경우
-				String keyModel = key.substring(0, 1).toUpperCase() + key.substring(1, key.length()-1);
-				String keyId = keyModel + "Id";
+				//String keyModel = key.substring(0, 1).toUpperCase() + key.substring(1, key.length()-1);
 				if(field.getAnnotation(ManyToMany.class) != null) {
-					/*String table = null;
-					table = ((JoinTable)field.getAnnotation(JoinTable.class)).name();
-					where += " AND id in (SELECT " + model + "Id FROM " + table + " WHERE " + keyId + "=:" + key + ")";*/
+					String joinTable = ((JoinTable)field.getAnnotation(JoinTable.class)).name();
+					Class<T> joinTableClass = (Class<T>) ((ParameterizedType) field.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+					PathBuilder<?> joinTablePath = new PathBuilder<>(joinTableClass, joinTable);
+					JPQLQuery<Long> subQuery = from(joinTablePath).select(joinTablePath.getNumber(modelName+"Id", Long.class))
+							.where(joinTablePath.get(key+"id").eq(params.get(key)));
+					query.where(entityPath.get("id").eq(subQuery));
+					//테스트 필요함
 				} else { //onetomany
 					// join keyModel on model.key = keyModel.key
 					//id in (select modelId from table where key=:key)
 					//where += " AND id in (SELECT " + model + "Id FROM " + keyModel + " WHERE " + id + "=:key)";
 				}
 			} else if(String.class.isAssignableFrom(field.getType())){ //문자열 검색
-				query.where(entityPath.getString(key).like(("%"+params.get(key)+"%").toLowerCase()));
+				query.where(entityPath.getString(key).lower().like(("%"+params.get(key)+"%").toLowerCase()));
 			/*} else if(Integer.class.isAssignableFrom(field.getType())){ //숫자 검색
 				;
 			} else if(Date.class.isAssignableFrom(field.getType())){ //날짜 검색
@@ -153,7 +136,15 @@ public abstract class GenericRepository<T extends Domain, ID extends Serializabl
 				query.where(entityPath.get(key).eq(params.get(key)));
 			}
 		}
-		return querydsl.applyPagination(pageable, query).fetch();
+		return query;
+	}
+
+	public List<T> listByKeys(Map<String, Object> params, Pageable pageable) {
+		return querydsl.applyPagination(pageable, listByKeys(params, (JPQLQuery<T>)from(entityPath))).fetch();
+	}
+	
+	public List<T> listByKeys(Map<String, Object> params) {
+		return listByKeys(params, (JPQLQuery<T>)from(entityPath)).fetch();
 	}
 	
 	public List<T> listByKeyId(String keyName, ID key) {
@@ -165,106 +156,51 @@ public abstract class GenericRepository<T extends Domain, ID extends Serializabl
 	}
 	
 	public List<T> listByKey(String keyName, ID key) {
-		if(key != null) {
-			String where = " WHERE " + keyName + "=:key";
-			return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).setParameter("key", key).getResultList();			
-		} else {
-			String where = " WHERE " + keyName + " is NULL";
-			return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).getResultList();
-		}
+		JPQLQuery<T> query = (JPQLQuery<T>)from(entityPath);
+		if(key != null) query.where(entityPath.get(keyName).eq(key));
+		else query.where(entityPath.get(keyName).isNull());
+		return query.fetch();
 	}
 	
 	public List<T> listByKey(String keyName, String key) {
-		if(key != null) {
-			String where = " WHERE " + keyName + "=:key";
-			return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).setParameter("key", key).getResultList();
-		} else {
-			String where = " WHERE " + keyName + " is NULL";
-			return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).getResultList();
-		}
-	}
-	
-	public List<T> listByKeys(Map<String, Object> params) {
-		String where = " WHERE 1=1";
-		for(String key : params.keySet()) {
-			Field field = Reflect.getField(domainClass, key);
-			if(field == null) { //검색되면 안되는 값. 필터링 해야 됨
-				params.remove(key);
-				continue; 
-			}
-			if(Model.class.isAssignableFrom(field.getType())) { //Type이 모델이라면
-				if(Code.class.isAssignableFrom(field.getType())) {	//Type이 Code일 경우
-					//params.put(key, Arrays.asList(key.split(",")));
-					params.put(key, Arrays.asList(((String)params.get(key)).split(",")));
-					where += " AND " + key + "Id IN (SELECT id FROM Code WHERE model='" + modelName + "' AND attribute='" + key + "' AND value IN :" + key +")";
-				} else  where += " AND " + key + "Id=:" + key;
-			} else if(Set.class.isAssignableFrom(field.getType())) { //Type이 List일 경우
-				String keyModel = key.substring(0, 1).toUpperCase() + key.substring(1, key.length()-1);
-				String keyId = keyModel + "Id";
-				if(field.getAnnotation(ManyToMany.class) != null) {
-					String table = null;
-					table = ((JoinTable)field.getAnnotation(JoinTable.class)).name();
-					where += " AND id in (SELECT " + modelName + "Id FROM " + table + " WHERE " + keyId + "=:" + key + ")";
-				} else { //onetomany
-					// join keyModel on model.key = keyModel.key
-					//id in (select modelId from table where key=:key)
-					//where += " AND id in (SELECT " + model + "Id FROM " + keyModel + " WHERE " + id + "=:key)";
-				}
-			} else if(String.class.isAssignableFrom(field.getType())){ //문자열 검색
-				params.put(key, ("%"+params.get(key)+"%").toLowerCase());
-				where += " AND LOWER(" + key + ") LIKE :" + key +"";
-			/*} else if(Integer.class.isAssignableFrom(field.getType())){ //숫자 검색
-				;
-			} else if(Date.class.isAssignableFrom(field.getType())){ //날짜 검색
-				;*/
-			} else where += " AND " + key + "=:" + key;
-			System.out.println(key);
-		}
-		Query query = em.createNativeQuery(SELECT_ALL_SQL + where, domainClass);
-		System.out.println(where);
-		for(String key : params.keySet()) query.setParameter(key, params.get(key));
-		return query.getResultList();
+		JPQLQuery<T> query = (JPQLQuery<T>)from(entityPath);
+		if(key != null) query.where(entityPath.get(keyName).eq(key));
+		else query.where(entityPath.get(keyName).isNull());
+		return query.fetch();
 	}
 	
 	public List<T> listByParent(ID parentId, Class<?> parentClass) {
-		String parentName = parentClass.getSimpleName();
+		String parentName = parentClass.getSimpleName().toLowerCase();
 		if(parentClass.equals(domainClass)) parentName = "parent";
-		String columnName = parentName.toLowerCase() + "Id";
-		if(parentId != null) {
-			String where = " WHERE " + columnName + "=:" + columnName;
-			return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).setParameter(columnName, parentId).getResultList();
-		} else {
-			String where = " WHERE " + columnName + " is NULL";
-			return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).getResultList();
-		}
+		JPQLQuery<T> query = (JPQLQuery<T>)from(entityPath);
+		if(parentId != null) query.where(entityPath.get(parentName).eq(parentId));
+		else query.where(entityPath.get(parentName).isNull());
+		return query.fetch();
 	}
 	
 	public List<T> listByParent(ID parentId, String parentName) {
-		String columnName = parentName.toLowerCase() + "Id";
-		if(parentId != null) {
-			String where = " WHERE " + columnName + "=:" + columnName;
-			return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).setParameter(columnName, parentId).getResultList();
-		} else {
-			String where = " WHERE " + columnName + " is NULL";
-			return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).getResultList();
-		}
+		parentName = parentName.toLowerCase();
+		JPQLQuery<T> query = (JPQLQuery<T>)from(entityPath);
+		if(parentId != null) query.where(entityPath.get(parentName).eq(parentId));
+		else query.where(entityPath.get(parentName).isNull());
+		return query.fetch();
 	}
 	
 	public List<T> search(String keyword, String[] keys) {
 		keyword = ("%" + keyword + "%").toLowerCase();
-		String where = " WHERE 1=1";
+		JPQLQuery<T> query = (JPQLQuery<T>)from(entityPath);
 		if(keys == null || keys.length == 0) keys = new String[]{"name"};
-		for (String key : keys) where += " AND LOWER(" + key + ") LIKE :keyword";
-		return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).setParameter("keyword", keyword).getResultList();
+		for (String key : keys) query.where(entityPath.getString(key).lower().like(("%"+keyword.toLowerCase()+"%")));
+		return query.fetch();
 	}
 	
 	public List<T> search(String keyword, String[] keys, ID userId) {
 		keyword = ("%" + keyword + "%").toLowerCase();
-		String where = " WHERE 1=1";
+		JPQLQuery<T> query = (JPQLQuery<T>)from(entityPath);
 		if(keys == null || keys.length == 0) keys = new String[]{"name"};
-		for (String key : keys) where += " AND LOWER(" + key + ") LIKE :keyword";
-		where += " AND userId = :userId";
-		return em.createNativeQuery(SELECT_ALL_SQL + where, domainClass).setParameter("keyword", keyword).setParameter("userId", userId).getResultList();
+		for (String key : keys) query.where(entityPath.getString(key).lower().like(("%"+keyword.toLowerCase()+"%")));
+		query.where(entityPath.get("userId").eq(userId));
+		return query.fetch();
 	}
 	
 	@Transactional
@@ -273,7 +209,7 @@ public abstract class GenericRepository<T extends Domain, ID extends Serializabl
 	}
 	 
 	public long count() {
-		return em.createNamedQuery(SELECT_COUNT_SQL, Long.class).getSingleResult();
+		return from(entityPath).fetchCount();
 	}
 	
 	@Transactional
